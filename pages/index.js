983d6ef61e84b3f320642b66a696b9d7b20f9927
thearnabsaha <thearnabsaha201@gmail.com>
2022-01-26 17:07:26 +0530

    s2

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
 import Head from 'next/head';
 import Weather from './Components/Weather';
-import Input from './Components/Input';
 const index = () => {
   return (
     <>
       <Head>
         <title>TAS Weather</title>
       </Head>
-      <Input/>
       <Weather/>
     </>
   );
