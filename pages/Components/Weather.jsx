089c5f7f1124831820a6558909d46758a8b27a90
thearnabsaha <thearnabsaha201@gmail.com>
2022-01-26 17:03:37 +0530

    s2

--- pages/Components/Weather.jsx
+++ pages/Components/Weather.jsx
@@ -1,17 +1,8 @@
-import { useState } from "react";
-
 const Weather = () => {
-    const [city, setCity] = useState("");
-    const handleChange=(e) => {
-        setCity(e.target.value)
-    }
     return (
-        <>
-            <form action="">
-                <input type="search" value={city} onChange={handleChange}/>
-                <button type="submit"></button>
-            </form>
-        </>
+        <div>
+            Enter
+        </div>
     );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Head from 'next/head';
 import Weather from './Components/Weather';
+import Input from './Components/Input';
 const index = () => {
   return (
     <>
       <Head>
         <title>TAS Weather</title>
       </Head>
+      <Input/>
       <Weather/>
     </>
   );
