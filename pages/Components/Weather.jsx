68cdd41208bcda516edf6fcf6f2927e1b42eb6be
thearnabsaha <thearnabsaha201@gmail.com>
2022-01-26 17:07:07 +0530

    s2

--- pages/Components/Weather.jsx
+++ pages/Components/Weather.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+import { MdSearch } from "react-icons/md";
 const Weather = () => {
+    const [city, setCity] = useState("");
+    const handleChange=(e) => {
+        setCity(e.target.value)
+    }
     return (
         <>
-            
+            <form action="">
+                <input type="search" value={city} onChange={handleChange}/>
+                <button type="submit"><MdSearch/></button>
+            </form>
         </>
     );
 }
